0719f3aded4bc0ee42a4b9e3daa37b6c9f604543
abhishektyagi1263 <abhishektyagi1263@gmail.com>
2021-02-15 11:15:31 +0530

    protype level 2 success release

--- codeblueapp/views.py
+++ codeblueapp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from codeblueapp.models import users
+#from codeblueapp.models import users
+from codeblueapp.forms import NewUser
 # Create your views here.
 
 def index(request):
     return render(request,'codeblueapp/index.html')
 
 def use(request):
-    use_list = users.objects.order_by('first_name')
-    use_dict = {'usem':use_list}
-    return render(request,'codeblueapp/users.html',context=use_dict,)
+    form = NewUser()
+
+    if request.method == 'POST':
+        form = NewUser(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('Error form invalid')
+    return render(request,'codeblueapp/users.html',{'form':form})
